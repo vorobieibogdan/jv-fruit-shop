4515e83ed5d48a4ca4363c5ffac44e6f8bf59dd8
Bohdan Vorobiei <vbs@ukr.net>
2026-03-25 15:27:11 +0200

    everything is complicated9

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,24 +1,12 @@
 package core.basesyntax;
 
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.DataConverter;
-import core.basesyntax.service.FileReaderService;
-import core.basesyntax.service.FileWriterService;
-import core.basesyntax.service.FruitShopService;
-import core.basesyntax.service.ReportGenerator;
-import core.basesyntax.service.impl.DataConverterImpl;
-import core.basesyntax.service.impl.FileReaderServiceImpl;
-import core.basesyntax.service.impl.FileWriterServiceImpl;
-import core.basesyntax.service.impl.FruitShopServiceImpl;
-import core.basesyntax.service.impl.ReportGeneratorImpl;
+import core.basesyntax.service.*;
+import core.basesyntax.service.impl.*;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
-import core.basesyntax.strategy.impl.BalanceOperation;
-import core.basesyntax.strategy.impl.OperationStrategyImpl;
-import core.basesyntax.strategy.impl.PurchaseOperation;
-import core.basesyntax.strategy.impl.ReturnOperation;
-import core.basesyntax.strategy.impl.SupplyOperation;
+import core.basesyntax.strategy.impl.*;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
